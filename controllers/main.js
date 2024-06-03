548a0c808f564707f9705bf332386397a6dbe71c
My Name <viverlin7@gmail.com>
2024-06-03 12:36:26 -0600

    authentication middleware refactor

--- controllers/main.js
+++ controllers/main.js
@@ -4,13 +4,13 @@ const CustomAPIError = require("../errors/custom-error");
 
 
 async function login(req, res) {
-    const {username, password} = req.body
+    const { username, password } = req.body
 
     // mongoose validations
     // Joi library
     // controller check as shown below
 
-    if (username.trim() === "" || password.trim() === "" ) {
+    if (username.trim() === "" || password.trim() === "") {
         throw new CustomAPIError("user and password cannot be blank", 400)
     }
 
@@ -19,29 +19,16 @@ async function login(req, res) {
     console.log(id)
 
 
-    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: "30d"})
-    return res.status(200).json({msg: `user created`, token})
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: "30d" })
+    return res.status(200).json({ msg: `user created`, token })
 }
 
 async function dashboard(req, res) {
-    const authHeader = req.headers.authorization
-    
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        throw new CustomAPIError("Auth token is invalid", 401)
-    }
 
-    const token = authHeader.split("Bearer ").pop()
-    const luckyNumber = Math.floor(Math.random()*100);
+    console.log("user obj", req.user);
 
-    try {
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        return res.status(200).json({msg: `hello ${decodedToken.username}`, secret: `Here is your authorized data, your lucky nmber is ${luckyNumber} `});
-
-    } catch(e) {
-        throw new CustomAPIError("Unathorized", 401);
-    }
-
-    
+    const luckyNumber = Math.floor(Math.random() * 100);
+    return res.status(200).json({ msg: `hello ${req.user.username}`, secret: `Here is your authorized data, your lucky nmber is ${luckyNumber} ` });
 }
 
-module.exports = {login, dashboard}
+module.exports = { login, dashboard }
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,29 @@
-
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
 
 const authenticationMiddleware = async (req, res, next) => {
-    console.log(req.headers.authorization)
-    next();
+
+    const authHeader = req.headers.authorization
+    
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        throw new CustomAPIError("Auth token is invalid", 401)
+    }
+
+    // space after bearer is needed
+    const token = authHeader.split("Bearer ").pop();
+
+    try {
+        // verify jason web token and decode, if token is correct add it to req object and pass it to next function.
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        console.log("decoded token",decodedToken)
+        const {id, username} = decodedToken
+        req.user = {id, username}
+        next();
+
+    } catch(e) {
+        throw new CustomAPIError("Unathorized", 401);
+    }
+
 }
 
 module.exports = authenticationMiddleware;
